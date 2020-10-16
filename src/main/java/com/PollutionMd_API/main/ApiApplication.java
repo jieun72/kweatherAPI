57d3b3c8a855df7bfa24b62ab08d94ee235e8842
Kwon\ksh <ksh_3115@kesti.co.kr>
2020-10-16 16:12:46 +0900

    JAR -> WAR

--- src/main/java/com/PollutionMd_API/main/ApiApplication.java
+++ src/main/java/com/PollutionMd_API/main/ApiApplication.java
@@ -2,9 +2,39 @@ package com.PollutionMd_API.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication extends SpringBootServletInitializer
+{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(ApiApplication.class);
+	}
+	
+	public static void main(String[] args) 
+	{
+		//run
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
+
+
+
+
+
+
+
+/*
+@SpringBootApplication
+public class ApiApplication 
+{
 
 	public static void main(String[] args) {
 		//run
@@ -12,3 +42,4 @@ public class ApiApplication {
 	}
 
 }
+*/
